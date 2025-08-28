43bc34c45fe63b73e570b182aae8e261804bb0e8
Jordan Avila Gomez <orstjordan18@gmail.com>
2025-08-28 14:53:20 -0600

    se modifica el pom para versiones y el model

--- src/main/java/com/nelys/pizza/bussines/domain/model/AreaModel.java
+++ src/main/java/com/nelys/pizza/bussines/domain/model/AreaModel.java
@@ -1,6 +1,17 @@
 package com.nelys.pizza.bussines.domain.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
 @Entity
-@Table
+@Table(name ="are")
+@Data
+
 public class AreaModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
 
 }
